0bd68ca6601420b05ba321dffbc90e3fc970b960
PRO X <bredalisgautreaux@gmail>
2024-08-27 08:31:13 +0200

    feat: ✨ Nuevos programas

---
+++ Metodos_Strings.py
@@ -0,0 +1,18 @@
+
+# Metodos de strings más usados
+
+palabra = "Hola, Mundo"
+
+print("Capitalize:",palabra.capitalize())
+print("Casefold:",palabra.casefold())
+print("Count:",palabra.count("o"))
+print("Find:",palabra.find("Hola"))
+print("Index:",palabra.index("Mundo"))
+print("Isalnum:",palabra.isalnum())
+print("Isalpha:",palabra.isalpha())
+print("Isascii:",palabra.isascii())
+print("Isdecimal:",palabra.isdecimal())
+print("Isdigit:",palabra.isdigit())
+print("Islower:",palabra.islower())
+print("Isnumeric:",palabra.isnumeric())
+print("Isprintable:",palabra.isprintable())
---
+++ Mostrar_Emojis.py
@@ -0,0 +1,15 @@
+
+# Imprimir distintos emojis
+
+print("1 - \U0001F602")
+print("10 - \U0001F601")
+print("11 - \U0001F603")
+print("100 - \U0001F604")
+print("101 - \U0001F605")
+print("110 - \U0001F606")
+print("111 - \U0001F607")
+print("1000 - \U0001F608")
+print("1001 - \U0001F609")
+print("1010 - \U0001F60D")
+print("1011 - \U0001F610")
+print("1100 - \U0001F612")
